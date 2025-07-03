c5650bb450d6fa80b967f47cde666595f26e4ea3
ryan horowitz <ryan.w.horowitz@gmail.com>
2025-07-03 17:44:52 -0400

    Remove default react App.js code.

--- shabbospdf-frontend/src/App.js
+++ shabbospdf-frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Shabbos Weather</h1>
       </header>
+      <main>
+        <div className="weather-container">
+          {/* Weather report will go here */}
+        </div>
+      </main>
     </div>
   );
 }
