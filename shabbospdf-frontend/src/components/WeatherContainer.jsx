f651bfc2ba64214e9694a842aa1d7ba62cdc9ddd
ryan horowitz <ryan.w.horowitz@gmail.com>
2025-07-03 19:44:36 -0400

    API key for openweather working

--- shabbospdf-frontend/src/components/WeatherContainer.jsx
+++ shabbospdf-frontend/src/components/WeatherContainer.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const WeatherContainer = () => {
+
+  useWeather();
   return (
     <div className="weather-content">
       <h2>Weather Report</h2>
@@ -8,5 +10,18 @@ const WeatherContainer = () => {
     </div>
   );
 };
+
+function useWeather() {
+  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  console.log('apikey', apiKey);
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Philadelphia,PA&APPID=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error('Error fetching weather:', error));
+  }, [apiKey]);
+}
 
 export default WeatherContainer; 
