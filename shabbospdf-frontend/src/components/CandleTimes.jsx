ffa9b2579d33c9d11fa6b5439fa82d9690898773
ryan horowitz <ryan.w.horowitz@gmail.com>
2025-07-03 18:47:49 -0400

    Candle times are being displayed.

--- shabbospdf-frontend/src/components/CandleTimes.jsx
+++ shabbospdf-frontend/src/components/CandleTimes.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const CandleTimes = () => {
+  useEffect(() => {
+    fetch('https://www.hebcal.com/shabbat?cfg=i2&zip=19147&ue=off&M=on&lg=s&tgt=_top')
+      .then(response => response.text())
+      .then(data => {
+        const hebcalElement = document.getElementById('hebcal-shabbat');
+        if (hebcalElement) {
+          hebcalElement.innerHTML = data;
+        }
+      })
+      .catch(error => console.error('Error fetching candle times:', error));
+  }, []);
+
   return (
     <div className="candle-times">
-      <h3>Candle Lighting Times</h3>
-      <p>Candle lighting information will be displayed here.</p>
+      <div id="hebcal-shabbat"></div>
     </div>
   );
 };
