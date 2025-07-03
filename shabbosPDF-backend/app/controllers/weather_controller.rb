fd426c8522fc5d1a6bbd3374082af3d1e2623c45
ryan horowitz <ryan.w.horowitz@gmail.com>
2025-07-03 16:22:11 -0400

    Add dotenv-rails gem and start writing weather API call

---
+++ shabbosPDF-backend/app/controllers/weather_controller.rb
@@ -0,0 +1,8 @@
+class WeatherController < ApplicationController
+  require 'httparty'
+
+  def friday_night
+    lat = 39.952815
+    lon = -75.1632153
+  end
+end
--- shabbosPDF-backend/config/routes.rb
+++ shabbosPDF-backend/config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get 'weather/friday_night', to: 'weather#friday_night'
 end
